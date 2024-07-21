5edd43c345a5fd1668aa06b5deba0586d442d22c
spspd <88385644+spspd@users.noreply.github.com>
2024-07-21 16:08:01 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a ;
+    string text;
+    cin >> a;
+    text = a==100 ? "pass" : "failure";
+    cout << text;
+    return 0;
+}
