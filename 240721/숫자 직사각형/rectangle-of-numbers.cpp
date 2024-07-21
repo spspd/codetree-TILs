b15ac4fe16e9e9be95fd7d2533e809dde55f4f40
spspd <88385644+spspd@users.noreply.github.com>
2024-07-21 17:04:05 +0900

    Title: 숫자 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/숫자 직사각형/rectangle-of-numbers.cpp
+++ 240721/숫자 직사각형/rectangle-of-numbers.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n=4, m= 3;
+    int n,m;
     int num = 1;
     
-    for(int i =0; i<4; i++){
-        for (int j = 0; j<3; j++){
+    cin >> n >> m;
+    for(int i =0; i<n; i++){
+        for (int j = 0; j<m; j++){
             cout << num<< " ";
             num += 1;
         }
