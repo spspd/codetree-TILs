76dd3702a00552a0c2d3cfcd00acc67dd288df6c
spspd <88385644+spspd@users.noreply.github.com>
2024-07-24 17:06:53 +0900

    Title: 대각선으로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/대각선으로 숫자 채우기/diagonal-numbering.cpp
+++ 240724/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -8,10 +8,13 @@ int main() {
     
     cin >> col >> row;
 
-    for(int i = 0; i < row; i++){
-        for(int j = i; 0 < j; j--){
-            chart[i][j] = cnt;
-            cnt++;
+    for(int i = 0; i < row +col -1; i++){
+        for(int j = i; 0 <= j; j--){
+            if( (i-j< col) && (j<row)){
+                chart[i-j][j] = cnt;
+                cnt++;
+
+            }
             // 0 / 01 10 / 02 11 20 
         }
     }
