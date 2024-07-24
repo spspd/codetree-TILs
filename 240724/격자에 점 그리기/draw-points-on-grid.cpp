0092725b4334db8b6484d50c0b3dd39a5ea158ec
spspd <88385644+spspd@users.noreply.github.com>
2024-07-24 17:56:06 +0900

    Title: 격자에 점 그리기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count;
+    cin >> n >> count;
+
+    int chart[10][10];
+    int cnt =1;
+
+    for(int i =0; i < count; i++){
+        int r,c;
+        cin >> r >> c;
+        chart[r][c] = cnt;
+        cnt++;
+    }
+
+    for(int i =0; i< n; i++){
+        for(int j =0; j < n; j++){
+            cout << chart[i+1][j+1] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
