971e2cc8a90e72c18abf886321a2c563ef47c5c2
spspd <88385644+spspd@users.noreply.github.com>
2024-07-24 17:23:55 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt =1;
+    cin >> n ; 
+
+    int chart[10][10] = {0};
+
+    for(int row = n-1; row>= 0 ; row--){
+        if(row % 2 == 0){
+            for(int col = 0; col < n ; col++){
+                chart[col][row] = cnt;
+                cnt += 1;
+            }
+        }
+        else{
+            for(int col = n-1; col>= 0 ; col--){
+                chart[col][row] = cnt;
+                cnt += 1;
+            }
+        }
+    }
+    for(int col = 0; col < n ; col ++){
+        for(int row = 0; row < n ; row++){
+            cout << chart[col][row] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
