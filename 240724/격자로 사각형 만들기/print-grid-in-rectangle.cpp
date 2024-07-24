fa7b17e9fabe3617b895ce04d4cccf457368b8a8
spspd <88385644+spspd@users.noreply.github.com>
2024-07-24 17:43:02 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row =0, col =0;
+    int chart[5][5] = {0};
+    for(int i = 0; i< 5 ; i++){
+        for(int j = 0; j< 5 ; j++){
+            if( i ==0 || j ==0){
+                chart[i][j] = 1;
+            }
+            else{
+                chart[i][j] = chart[i-1][j]+ chart[i][j-1] + chart[i-1][j-1];
+            }
+        }
+    }
+    for(int i = 0 ; i< 5 ; i++){
+        for(int j =0; j<5 ; j++){
+            cout << chart[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
