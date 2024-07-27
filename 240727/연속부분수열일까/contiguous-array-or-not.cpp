bb8445fe18ee79dd426f53532875cea75e4c335e
spspd <88385644+spspd@users.noreply.github.com>
2024-07-27 09:41:41 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240727/연속부분수열일까/contiguous-array-or-not.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 설정
+    int n1, n2 ;
+
+    int n1_array[100] = {0};
+    int n2_array[100] = {0};
+
+    for( int i = 0; i< n1; i++){
+        cin >> n1_array[i];
+    }
+
+    for( int i = 0; i< n2; i++){
+        cin >> n2_array[i];
+    }
+
+    // 비교 ( B가 A의  -> A가 같거나 더 큼)
+
+    // 초기화 
+    int correct = -1;
+    for( int i =0; i< n1 ; i++){
+        for (int j = 0; j< n2; j++){
+
+            // 더 적게 남았다면
+            if(n1 -i < n2){
+                break;
+            }
+
+            // 같지 않다면 
+            if(n1_array[i+j] != n2_array[j]){
+                break;
+            }
+            else{
+                // clear
+                if(j == n2 -1){
+                    correct = 1;
+                }
+            }
+        }
+
+    }
+
+    cout << (correct == 1 ? "Yes" : "No" )<< endl;
+    return 0;
+}
