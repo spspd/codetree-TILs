c2345cec125151330863013617207f5ad946a8a4
spspd <88385644+spspd@users.noreply.github.com>
2024-11-26 13:15:47 +0900

    Title: 문자에 따른 명령 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241126/문자에 따른 명령 2/text-based-commands2.cpp
+++ 241126/문자에 따른 명령 2/text-based-commands2.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 int dir_name = 3;
-int dx[4] = {1,-1,0,0};
-int dy[4] = {0,0,-1,1};
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,-1,0,1};
 int xpos =0; int ypos = 0;
 int main() {
     // 여기에 코드를 작성해주세요.
     string a;
     cin >>a;
     for(char k : a){
-        if(k == 'L'){dir_name =1;}
-        if(k == 'R'){dir_name =0;}
+        if(k == 'L'){
+            dir_name+=3;
+            dir_name%=4;
+            }
+        if(k == 'R'){
+            dir_name+=1;
+            dir_name%=4;}
         if(k == 'F'){
             xpos+= dx[dir_name];
             ypos+= dy[dir_name];
