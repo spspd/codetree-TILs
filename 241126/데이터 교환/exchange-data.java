e4b1f239cc108e04f82faae102f091d67d11f11b
spspd <88385644+spspd@users.noreply.github.com>
2024-11-26 14:07:00 +0900

    Title: 데이터 교환, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 241126/데이터 교환/exchange-data.java
+++ 241126/데이터 교환/exchange-data.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 6, c =7;
-        int temp = c;
-        b = a;
+        int temp = a;
+        a = c;
         c = b;
-        a = temp;
+        b = temp;
         System.out.print(a+"\n"+b+"\n"+c);
         
     }
