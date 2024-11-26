b646dab3888f7a69732d2ee0eacac5b54b983296
spspd <88385644+spspd@users.noreply.github.com>
2024-11-26 13:59:16 +0900

    Title: 변수 출력하기 3, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241126/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 1, b = 2;
+        char c = 'C';
+        System.out.print(a);
+        System.out.print("->");
+        System.out.print(b);
+        System.out.print("->");
+        System.out.print(c);
+    }
+}
