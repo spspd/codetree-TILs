5b5c5ac53a608820402ab8ebbc14e498e226abfb
spspd <88385644+spspd@users.noreply.github.com>
2024-11-26 14:06:03 +0900

    Title: 데이터 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241126/데이터 교환/exchange-data.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c =7;
+        int temp = c;
+        b = a;
+        c = b;
+        a = temp;
+        System.out.print(a+"\n"+b+"\n"+c)
+        
+    }
+}
