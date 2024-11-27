bfa6bdfa7bc2e271a4071e64acb83677388f0cd7
spspd <88385644+spspd@users.noreply.github.com>
2024-11-27 14:31:10 +0900

    Title: 입력받은 값과 합 출력, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241127/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(a + " " + b + " " + (a+b) );
+        
+        
+        bw.close();
+
+        
+    }
+}
